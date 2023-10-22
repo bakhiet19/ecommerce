462a5c8132f4d2ff09185901cb7bc8d2e44aed68
bakhiet19 <63530204+bakhiet19@users.noreply.github.com>
2023-10-22 16:06:57 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 const bars = document.querySelector('.bars');
 const nav = document.querySelector('.nav')
 const close = document.querySelector('.close')
+const img = document.querySelector('.single-pro-image img')
+const imgs = document.querySelectorAll('.small-img-col img');
+
+imgs.forEach(function(e){
+    e.addEventListener('click' , function(){
+        img.setAttribute('src' , e.src)
+    })
+})
+
 
 
 
@@ -11,3 +20,4 @@ bars.addEventListener('click' , function(){
 close.addEventListener('click' , function(){
     nav.style.right = '-300px';
 })
+
