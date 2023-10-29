3186f069e62f8464fea8991998a5cd28c8f75538
bakhiet19 <63530204+bakhiet19@users.noreply.github.com>
2023-10-29 23:53:23 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,72 @@ close.addEventListener('click' , function(){
     nav.style.right = '-300px';
 })
 
+
+
+
+
+
+
+
+const a = ["./assets/img/products/f5.jpg" , "./assets/img/products/f6.jpg" , "./assets/img/products/f7.jpg" , "./assets/img/products/f8.jpg" , "./assets/img/products/f3.jpg" , "./assets/img/products/f1.jpg"];
+let imgs_slider = document.getElementById('img');
+let left = document.querySelector('#left');
+let right = document.querySelector('#right');
+var count = 0;
+
+
+
+
+right.addEventListener('click' , function(e){
+    if(count == a.length - 1){
+        count = 0
+        imgs_slider.setAttribute('src' , a[count])
+    }
+    else{
+     ++ count;
+     imgs_slider.setAttribute('src' , a[count])
+    }
+ })
+
+
+
+
+
+left.addEventListener('click' , function(e){
+   if(count == 0){
+    count = a.length -1
+    imgs_slider.setAttribute('src' , a[count])
+   }
+   else{
+    -- count;
+    imgs_slider.setAttribute('src' , a[count])
+   }
+})
+
+
+
+
+
+
+// let one = document.getElementById('one');
+// let two = document.getElementById('two');
+// let three = document.getElementById('three');
+// let four = document.getElementById('four');
+
+
+// one.addEventListener('click' , function(){
+//     imgs_slider.setAttribute('src' , a[0])
+// })
+
+// two.addEventListener('click' , function(){
+//     imgs_slider.setAttribute('src' , a[1])
+// })
+
+
+// three.addEventListener('click' , function(){
+//     imgs_slider.setAttribute('src' , a[2])
+// })
+
+// four.addEventListener('click' , function(){
+//     imgs_slider.setAttribute('src' , a[3])
+// })
